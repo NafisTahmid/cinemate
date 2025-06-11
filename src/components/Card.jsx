a40f5b8b9c1ac574829566e1226d51e859a6c7a1
unknown <nafistahmid4@gmail.com>
2025-06-11 11:02:30 +0600

    Second commit

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -13,7 +13,7 @@ function Card({movie}) {
             <Link to={`/movies/${id}`}>
                 <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{original_title}</h5>
             </Link>
-            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{overview}</p>
+            <p className="mb-3 font-normal text-justify text-base/6 text-gray-700 dark:text-gray-400">{overview}</p>
         </div>
     </div>
   )
